b03216bb626acadb49ecccd1a81b1cf0482ac686
haza-kumara <hazakumaraaa@gmail.com>
2026-05-10 17:04:44 +0700

    feat: add windowed app using Electron

---
+++ main.js
@@ -0,0 +1,24 @@
+const {app, BrowserWindow } = require('electron')
+const path = require('path')
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 500,
+        height: 500,
+        resizable: false,
+        titleBarStyle: 'hiddenInset',
+        backgroundColor: '#FAF9F7',
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false
+        }
+    })
+
+    win.loadFile('index.html')
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+    if(process.platform !== 'darwin') app.quit
+})
